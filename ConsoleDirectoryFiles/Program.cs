b3f96491d037239382579ce1d15ba4b4a61c717e
Lerno Baklan <yak.kok.93@inbox.ru>
2022-05-11 18:17:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleDirectoryFiles/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDirectoryFiles
+{
+    [System.Serializable]
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class Program : SystemException
+    {
+
+        private static DirectoryInfo _rootDirectory;
+        private static string[] _specDirectories = new string[] { "Изображения", "Документы", "Прочее" };
+        private static int _imageCount = 0, _documentCount = 0, _otherCount = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к диску:");
+            string directoryPath = Console.ReadLine();
+            
+            var driveInfo = new DriveInfo(directoryPath);
+            Console.WriteLine($"Информация о диске:{driveInfo.VolumeLabel}, всего {driveInfo.TotalSize /1024/1024} Мб,"+
+                $"свободно {driveInfo.AvailableFreeSpace /1024/1024} Мб.");
+            _rootDirectory = driveInfo.RootDirectory;
+            SearchDirectories(_rootDirectory);
+
+            foreach (var directory in _rootDirectory.GetDirectories())
+            {
+                if (!_specDirectories.Contains(directory.Name))
+                    directory.Delete(true);
+            }
+
+            var resultText = $"Всего обработано {_imageCount+_documentCount+_otherCount} файлов."+
+                $"Из них {_imageCount} изображений, {_documentCount} документов, {_otherCount} прочих файлов";
+            Console.WriteLine(resultText);
+            File.WriteAllText(_rootDirectory + "\\Инфо.txt", resultText);
+
+            Console.ReadLine();
+        }
+        
+        private static void SearchDirectories(DirectoryInfo currentDirectory)
+        {
+            if (!_specDirectories.Contains(currentDirectory.Name))
+            {
+                FilterFiles(currentDirectory);
+                foreach (var childDirectory in currentDirectory.GetDirectories())
+                {
+                    SearchDirectories(childDirectory);
+                }
+            }
+        }
+        private static void FilterFiles(DirectoryInfo currentDirectory)
+        {
+            var currentFiles = currentDirectory.GetFiles();
+
+             
+            foreach (var fileInfo in currentFiles)
+            {
+                if (new string[] { ".jpg", ".png", ".jpeg", ".bmp" }
+                .Contains(fileInfo.Extension.ToLower()))
+                {
+                    var photoDirectory = new DirectoryInfo(_rootDirectory + $"{_specDirectories[0]}\\");
+                    if (!photoDirectory.Exists)
+                        photoDirectory.Create();
+                    
+
+                    
+                    var yearDirectory = new DirectoryInfo(photoDirectory + $"{fileInfo.LastWriteTime.Date.Year}\\");
+                    if (!yearDirectory.Exists)
+                        yearDirectory.Create();
+
+                   
+                    MoveFile(fileInfo, yearDirectory);
+                    _imageCount++;
+                }
+                 
+            }
+        }
+
+        private static void MoveFile(FileInfo fileInfo, DirectoryInfo directoryInfo)
+        {
+            var newFileInfo = new FileInfo(directoryInfo + $"\\{fileInfo.Name}");
+            while (!newFileInfo.Exists)
+                newFileInfo = new FileInfo(directoryInfo + $"\\{Path.GetFileNameWithoutExtension(fileInfo.FullName)}(1)" +
+                    $"{newFileInfo.Extension}");
+            fileInfo.MoveTo(newFileInfo.FullName);
+        }
+        
+    }
+    
+}
---
+++ ConsoleDirectoryFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleDirectoryFiles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleDirectoryFiles")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4180cb77-bdaf-4090-a44d-c0116ba0a84e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
